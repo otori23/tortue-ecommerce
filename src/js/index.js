09d7207151124a8af7e1b357e68dc117783c3917
otori23 <azeez.otori@gmail.com>
2020-02-27 10:03:49 -0800

    working on showcase section

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,19 @@
 import '../css/style.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 (() => {
   window.addEventListener('DOMContentLoaded', e => {
-    let wrapper = document.getElementById('wrapper');
-    let topLayer = wrapper.querySelector('.top');
-    let handle = wrapper.querySelector('.handle');
+    let showcase = document.getElementById('showcase');
+    let topLayer = showcase.querySelector('.top');
+    let handle = showcase.querySelector('.handle');
     let skew = 0;
     let delta = 0;
 
-    if (wrapper.className.indexOf('skewed') != -1) {
+    if (showcase.className.indexOf('skewed') != -1) {
       skew = 1000;
     }
 
-    wrapper.addEventListener('mousemove', e => {
+    showcase.addEventListener('mousemove', e => {
       delta = (e.clientX - window.innerWidth / 2) * 0.5;
 
       handle.style.left = e.clientX + delta + 'px';
--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,15 @@ module.exports = {
       {
         test: /\.html$/i,
         loader: 'html-loader'
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'fonts/[name].[ext]'
+          }
+        }
       }
     ]
   },
